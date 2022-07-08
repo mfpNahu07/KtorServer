739726bf2b70d13237616f9542e682958c03610a
Nahue Imaz <nahuelji.773@gmail.com>
2022-07-08 15:10:40 -0300

    Se crea el servidor

---
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -0,0 +1,26 @@
+package com.example.plugins
+
+import com.example.routes.getAllHeroes
+import com.example.routes.root
+import com.example.routes.searchHeroes
+import io.ktor.application.*
+import io.ktor.http.content.*
+import io.ktor.response.*
+import io.ktor.routing.*
+
+
+fun Application.configureRouting() {
+
+    //Aca vamos a poner todas las rutas para este server, los "endpoints"
+
+
+    routing {
+        root()
+        getAllHeroes()
+        searchHeroes()
+
+        static("/images"){
+            resources("images")
+        }
+    }
+}
