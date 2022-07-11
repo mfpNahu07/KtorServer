a2b6b3dbe81b03216544f2f20842ed63fb4a7f5f
Nahue Imaz <nahuelji.773@gmail.com>
2022-07-11 18:33:19 -0300

    Se continuaron los tests

--- src/test/kotlin/com/example/ApplicationTest.kt
+++ src/test/kotlin/com/example/ApplicationTest.kt
@@ -2,6 +2,7 @@ package com.example
 
 
 //import io.ktor.server.routing.*
+import com.example.models.ApiResponse
 import io.ktor.http.*
 //import io.ktor.serialization.kotlinx.json.*
 //import io.ktor.server.plugins.contentnegotiation.*
@@ -15,12 +16,23 @@ import io.ktor.client.statement.*
 import kotlin.test.*
 import io.ktor.server.testing.*
 import com.example.plugins.*
+import com.example.repository.HeroRepository
+import com.example.repository.NEXT_PAGE_KEY
+import com.example.repository.PREVIOUS_PAGE_KEY
 import io.ktor.application.*
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
+import org.koin.java.KoinJavaComponent.inject
+import kotlinx.serialization.decodeFromString
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class ApplicationTest {
 
+    private val heroRepository: HeroRepository by inject(HeroRepository::class.java)
+
     @Test
-    fun testRoot() {
+    fun `access root endpoint, assert correct information`() {
         withTestApplication(moduleFunction = Application::module) {
             handleRequest(HttpMethod.Get, "/").apply {
                 assertEquals(
@@ -34,4 +46,121 @@ class ApplicationTest {
             }
         }
     }
+
+
+    @ExperimentalSerializationApi
+    @Test
+    fun `access all heroes endpoint, query all pages, assert correct information`() {
+        withTestApplication(moduleFunction = Application::module) {
+            val pages = 1..5
+            val heroes = listOf(
+                heroRepository.page1,
+                heroRepository.page2,
+                heroRepository.page3,
+                heroRepository.page4,
+                heroRepository.page5
+            )
+            pages.forEach { page ->
+                handleRequest(HttpMethod.Get, "/boruto/heroes?page=$page").apply {
+                    assertEquals(
+                        expected = HttpStatusCode.OK,
+                        actual = response.status()
+                    )
+
+                    val expected = ApiResponse(
+                        success = true,
+                        message = "Ok",
+                        prevPage = calculatePage(page = page)["prevPage"],
+                        nextPage = calculatePage(page = page)["nextPage"],
+                        heroes = heroes[page - 1],
+                    )
+                    val actual = Json.decodeFromString<ApiResponse>(response.content.toString())
+                    assertEquals(
+                        expected = expected,
+                        actual = actual
+                    )
+                }
+            }
+        }
+    }
+
+
+
+
+    @ExperimentalSerializationApi
+    @Test
+    fun `access all heroes endpoint, query non existing page number, assert error`() {
+        withTestApplication(moduleFunction = Application::module) {
+            handleRequest(HttpMethod.Get, "/boruto/heroes?page=6").apply {
+                assertEquals(
+                    expected = HttpStatusCode.NotFound,
+                    actual = response.status()
+                )
+                val expected = ApiResponse(
+                    success = false,
+                    message = "Heroes not found"
+                )
+                val actual = Json.decodeFromString<ApiResponse>(response.content.toString())
+                assertEquals(
+                    expected = expected,
+                    actual = actual
+                )
+            }
+        }
+    }
+
+    @ExperimentalSerializationApi
+    @Test
+    fun `access all heroes endpoint, query invalid page number, assert error`() {
+        withTestApplication(moduleFunction = Application::module) {
+            handleRequest(HttpMethod.Get, "/boruto/heroes?page=invalid").apply {
+                assertEquals(
+                    expected = HttpStatusCode.BadRequest,
+                    actual = response.status()
+                )
+                val expected = ApiResponse(
+                    success = false,
+                    message = "Only numbers allowed"
+                )
+                val actual = Json.decodeFromString<ApiResponse>(response.content.toString())
+                assertEquals(
+                    expected = expected,
+                    actual = actual
+                )
+            }
+        }
+    }
+
+
+
+    @ExperimentalSerializationApi
+    @Test
+    fun `access non existing endpoint,assert not found`() {
+        withTestApplication(moduleFunction = Application::module) {
+            handleRequest(HttpMethod.Get, "/unknown").apply {
+                assertEquals(expected = HttpStatusCode.NotFound, actual = response.status())
+                assertEquals(expected = "Page not found.", actual = response.content)
+            }
+        }
+    }
+
+    private fun calculatePage(page: Int): Map<String, Int?> {
+        var prevPage: Int? = page
+        var nextPage: Int? = page
+        if (page in 1..4) {
+            nextPage = nextPage?.plus(1)
+        }
+        if (page in 2..5) {
+            prevPage = prevPage?.minus(1)
+        }
+        if (page == 1) {
+            prevPage = null
+        }
+        if (page == 5) {
+            nextPage = null
+        }
+        return mapOf(PREVIOUS_PAGE_KEY to prevPage, NEXT_PAGE_KEY to nextPage)
+    }
+
+
 }
